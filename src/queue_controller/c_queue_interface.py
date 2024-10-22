d361f252d007d209af92316b9f57bf3da5cc50fd
Emil <me@emilvinu.de>
2024-10-22 10:12:05 +0200

    implemented mongodb initialization

--- src/queue_controller/c_queue_interface.py
+++ src/queue_controller/c_queue_interface.py
@@ -1,11 +1,16 @@
-import pymongo
+from pymongo import MongoClient
 from c_queue_item import QueueItem
 
 
 class QueueInterface:
-    def __init__(self) -> None:
-        # connect to mongodb
-        pass
+    def __init__(self, mongo_uri: str, db_name: str) -> None:
+        self.mongo_client = MongoClient(mongo_uri)
+        self.db = self.mongo_client(db_name)
+        self.db_training_queue = self.db["training_queue"]
+        self.db_models = self.db["models"]
 
-    def get_queue_item(self) -> QueueItem:
-        pass
+    def get_queue_item(self) -> QueueItem | None:
+        queue_item = self.db_training_queue.find_one_and_delete({})
+        if queue_item is None:
+            return queue_item
+        return QueueItem(queue_item)
--- src/queue_controller/c_queue_item.py
+++ src/queue_controller/c_queue_item.py
@@ -1,2 +1,11 @@
 class QueueItem:
-    pass
+    def __init__(self, document: dict) -> None:
+        self.status = document.status
+        self.output = document.output
+        self.task = document.task
+        self.last_updated_at = document.last_updated_at
+        self.queued_at = document.queued_at
+        self.started_at = document.started_at
+        self.finished_at = document.finished_at
+        self.error = document.error
+        self.project_id = document.project_id
