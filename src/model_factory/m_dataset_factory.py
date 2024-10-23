b92ec5f1ad28fe7bc1cad5b5d9c0909b5aa995da
servant-of-scietia <samsun2006@icloud.com>
2024-10-24 00:09:47 +0200

    normalization routine

--- src/model_factory/m_dataset_factory.py
+++ src/model_factory/m_dataset_factory.py
@@ -6,13 +6,16 @@ def call(self, operation: dict) -> None:
     if operation["method"] == "new":
         ds = tfds.load(
             operation["args"]["class"],
-            operation["args"]["split"],
+            split=operation["args"]["split"],
             shuffle_files=True,
+            as_supervised=True,
         )
-        if "normalization" in operation["args"]["preprocess"]:
+        ds = ds.batch(operation["args"]["batch_size"])
+        assert isinstance(ds, tf.data.Dataset)
+        if "normalize" in operation["args"]["preprocess"]:
             layer = keras.layers.Normalization()
-            layer.adapt(ds)
-            ds = layer(ds)
+            layer.adapt(ds.map(lambda x, y: x))
+            ds = ds.map(lambda x, y: (layer(x), y))
 
         # expand with more preprocessing methods
 
