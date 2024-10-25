2dc9eb272368d0bd66221b4d7eb9daf7632c710a
servant-of-scietia <samsun2006@icloud.com>
2024-10-25 14:46:57 +0200

    add input layers

--- src/model_factory/layer_factories/f_input_factory.py
+++ src/model_factory/layer_factories/f_input_factory.py
@@ -6,4 +6,10 @@ from keras.src.utils.module_utils import tensorflow
 def call(self, operation: dict) -> None:
     self.project_data[operation["uid"]] = keras.layers.Input(
         shape=tuple(x for x in operation["args"]["shape"]),
+        batch_size=(operation["args"]["batch_size"] if "batch_size" in operation["args"] else None),
+        dtype=(operation["args"]["dtype"] if "dtype" in operation["args"] else None),
+        sparse=(operation["args"]["sparse"] if "sparse" in operation["args"] else False),
+        name=(operation["args"]["name"] if "name" in operation["args"] else None),
+        tensor=(self.project_data[operation["args"]["tensor"]] if "tensor" in operation["args"] else None),
+        optional=(operation["args"]["optional"] if "optional" in operation["args"] else False),
     )
