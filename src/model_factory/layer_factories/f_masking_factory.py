9dae507954610c97fa6c422b893d83f6369803bf
servant-of-scietia <samsun2006@icloud.com>
2024-10-25 21:03:24 +0200

    masking layer

---
+++ src/model_factory/layer_factories/f_masking_factory.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import keras
+
+
+def call(self, operation: dict) -> None:
+    self.project_data[operation["uid"]] = keras.layers.Masking(
+        (operation["args"]["mask_value"] if "mask_value" in operation["args"] else 0.0),
+    )(self.project_data[operation["args"]["inputs"]])
