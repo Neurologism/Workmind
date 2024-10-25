64a838e9ce8062db9c8d866343809569995370f5
servant-of-scietia <samsun2006@icloud.com>
2024-10-25 20:59:10 +0200

    embedding layer & marking stuff as not complete

--- src/model_factory/layer_factories/f_input_factory.py
+++ src/model_factory/layer_factories/f_input_factory.py
@@ -9,7 +9,9 @@ def call(self, operation: dict) -> None:
         batch_size=(operation["args"]["batch_size"] if "batch_size" in operation["args"] else None),
         dtype=(operation["args"]["dtype"] if "dtype" in operation["args"] else None),
         sparse=(operation["args"]["sparse"] if "sparse" in operation["args"] else False),
+        batch_shape=(tuple(x for x in operation["args"]["batch_shape"]) if "batch_shape" in operation["args"] else None),
         name=(operation["args"]["name"] if "name" in operation["args"] else None),
-        tensor=(self.project_data[operation["args"]["tensor"]] if "tensor" in operation["args"] else None),
         optional=(operation["args"]["optional"] if "optional" in operation["args"] else False),
     )
+
+# not complete
--- src/model_factory/layer_factories/f_input_spec_factory.py
+++ src/model_factory/layer_factories/f_input_spec_factory.py
@@ -10,8 +10,9 @@ def call(self, operation: dict) -> None:
         ndim=(operation["args"]["ndim"] if "ndim" in operation["args"] else None),
         max_ndim=(operation["args"]["max_ndim"] if "max_ndim" in operation["args"] else None),
         min_ndim=(operation["args"]["min_ndim"] if "min_ndim" in operation["args"] else None),
-        axes=(operation["args"]["axes"] if "axes" in operation["args"] else None),
         allow_last_axis_squeeze=(operation["args"]["allow_last_axis_squeeze"] if "allow_last_axis_squeeze" in operation["args"] else None),
         name=(operation["args"]["name"] if "name" in operation["args"] else None),
         optional=(operation["args"]["optional"] if "optional" in operation["args"] else False),
     )
+
+# not complete
