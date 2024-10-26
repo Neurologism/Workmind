c9c913da909f5bfb7577e264dfb4e003887a692e
servant-of-scietia <samsun2006@icloud.com>
2024-10-26 18:32:11 +0200

    image augmentation layers

---
+++ src/model_factory/layer_factories/preprocessing_factories/f_randombrightness_factory.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import keras
+
+
+def call(self, operation: dict) -> None:
+    self.project_data[operation["uid"]] = keras.layers.RandomBrightness(
+        factor=operation["args"]["factor"],
+        value_range=(tuple(operation["args"]["value_range"]) if "value_range" in operation["args"] else (0, 255)),
+        seed=(operation["args"]["seed"] if "seed" in operation["args"] else None),
+    )(self.project_data[operation["args"]["inputs"]])
---
+++ src/model_factory/layer_factories/preprocessing_factories/f_randomflip_factory.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import keras
+
+
+def call(self, operation: dict) -> None:
+    self.project_data[operation["uid"]] = keras.layers.RandomFlip(
+        mode=(operation["args"]["mode"] if "mode" in operation["args"] else "horizontal_and_vertical"),
+        seed=(operation["args"]["seed"] if "seed" in operation["args"] else None),
+        data_format=(operation["args"]["data_format"] if "data_format" in operation["args"] else None),
+    )(self.project_data[operation["args"]["inputs"]])
