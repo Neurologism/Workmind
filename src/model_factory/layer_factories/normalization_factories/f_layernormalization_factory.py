9dc934671ad4c1c16f88b5e3d24a857af315dfab
servant-of-scietia <samsun2006@icloud.com>
2024-10-26 18:41:47 +0200

    normalization layers

---
+++ src/model_factory/layer_factories/normalization_factories/f_layernormalization_factory.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import keras
+
+
+def call(self, operation: dict) -> None:
+    self.project_data[operation["uid"]] = keras.layers.LayerNormalization(
+        axis=(operation["args"]["axis"] if "axis" in operation["args"] else -1),
+        epsilon=(operation["args"]["epsilon"] if "epsilon" in operation["args"] else 1e-3),
+        center=(operation["args"]["center"] if "center" in operation["args"] else True),
+        scale=(operation["args"]["scale"] if "scale" in operation["args"] else True),
+        beta_initializer=(operation["args"]["beta_initializer"] if "beta_initializer" in operation["args"] else "zeros"),
+        gamma_initializer=(operation["args"]["gamma_initializer"] if "gamma_initializer" in operation["args"] else "ones"),
+    )(self.project_data[operation["args"]["inputs"]])
