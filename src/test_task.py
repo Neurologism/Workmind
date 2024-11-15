99c116b49c7b796354a5202b301a079747eb5e26
Emil <me@emilvinu.de>
2024-11-15 19:22:02 +0100

    changed task.json reference in test_task.json

--- src/test_task.py
+++ src/test_task.py
@@ -2,5 +2,5 @@ from tensorflow_middleware import WhitemindProject
 
 if __name__ == "__main__":
     a = WhitemindProject()
-    a.read_json("../task.json")
+    a.read_json("task.json")
     a.execute()
