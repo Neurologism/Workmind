6c5e36aa14f21faef8c174466729cb582430f44c
Emil <me@emilvinu.de>
2024-12-13 20:42:19 +0100

    changed error handler to retry if exception during exception handling

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import dotenv
 import os
 from queue_controller import QueueInterface
+import time
 
 dotenv.load_dotenv()
 
@@ -19,5 +20,12 @@ if __name__ == "__main__":
         except Exception as e:
             print(f"Error during training: {e}")
             print("Reloading queue interface...")
-            qi = QueueInterface(MONGO_URI, DB_NAME)
-            qi.requeue_abandoned_trainigs()
+            while True:
+                try:
+                    qi = QueueInterface(MONGO_URI, DB_NAME)
+                    qi.requeue_abandoned_trainigs()
+                    break
+                except Exception as e:
+                    print(f"Error during reloading: {e}")
+                    print("Sleeping 10 seconds...")
+                    time.sleep(10)
--- src/queue_controller/c_queue_interface.py
+++ src/queue_controller/c_queue_interface.py
@@ -117,36 +117,33 @@ class QueueInterface:
     def requeue_abandoned_trainigs(self) -> None:
         print("Searching for abandoned trainings...")
         found = False
-        try:
-            for model in self.db_models.find({"status": "training"}):
-                lastUpdated = datetime.strptime(
-                    model["datelastUpdated"], "%Y-%m-%dT%H:%M:%S.%fZ"
-                ).replace(tzinfo=timezone.utc)
-                if lastUpdated < datetime.now(timezone.utc) - timedelta(minutes=1):
-                    self.db_models.update_one(
-                        {"_id": model["_id"]},
-                        {
-                            "$set": {
-                                "status": "queued",
-                                "datelastUpdated": datetime.now(timezone.utc).strftime(
-                                    "%Y-%m-%dT%H:%M:%S.%f"
-                                )[:-3]
-                                + "Z",
-                            }
-                        },
-                    )
-
-                    self.db_training_queue.insert_one(
-                        {
-                            "taskId": model["_id"],
+        for model in self.db_models.find({"status": "training"}):
+            lastUpdated = datetime.strptime(
+                model["datelastUpdated"], "%Y-%m-%dT%H:%M:%S.%fZ"
+            ).replace(tzinfo=timezone.utc)
+            if lastUpdated < datetime.now(timezone.utc) - timedelta(minutes=1):
+                self.db_models.update_one(
+                    {"_id": model["_id"]},
+                    {
+                        "$set": {
+                            "status": "queued",
+                            "datelastUpdated": datetime.now(timezone.utc).strftime(
+                                "%Y-%m-%dT%H:%M:%S.%f"
+                            )[:-3]
+                            + "Z",
                         }
-                    )
+                    },
+                )
 
-                    found = True
+                self.db_training_queue.insert_one(
+                    {
+                        "taskId": model["_id"],
+                    }
+                )
 
-            if found:
-                print("Found and requeued abandoned trainings.")
-            else:
-                print("No abandoned trainings found.")
-        except Exception as e:
-            print(f"Error during requeueing abandoned trainings, aborting: {e}")
+                found = True
+
+        if found:
+            print("Found and requeued abandoned trainings.")
+        else:
+            print("No abandoned trainings found.")
