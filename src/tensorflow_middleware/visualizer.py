4155e0046ce76518b77a30b60b2dda040bf7c6e7
Servant-of-Scietia <samsun2006@icloud.com>
2025-01-21 22:51:44 +0100

    new visualizer

--- src/tensorflow_middleware/visualizer.py
+++ src/tensorflow_middleware/visualizer.py
@@ -1,1 +1,17 @@
-visualizer_to_function = {}
+def line_chart(params: dict):
+    params["logger"][params["id"]] = [
+        {
+            "x": params["x_label"],
+            "y": params["y_label"],
+            "val_y": "val_" + params["y_label"],
+        },
+        {
+            "x_label": params["x_label"],
+            "y_label": params["y_label"],
+        },
+    ]
+
+
+visualizer_to_function = {
+    "line-chart": line_chart,
+}
